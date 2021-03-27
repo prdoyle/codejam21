d9dfa0a233a78994558a55144bd8b690c57ad5e4
Patrick Doyle <pdoyle@venacorp.com>
2021-03-27 11:05:23 -0400

    Final mediansort

--- qual/mediansort/mediansort.py
+++ qual/mediansort/mediansort.py
@@ -1,14 +1,18 @@
 #! /usr/bin/python
 
-from sys import stdin, stdout, argv
+from sys import stdin, stdout, stderr, argv
 
+log_file = open( "log.mediansort", "a" )
+log_file.write( "\n------------------\n" )
 def log( string ):
-    print( " > " + string )
+    pass
+    #log_file.write( " > %s\n" % string )
 
 def median_sorted( num_elements, median_of ):
-    if num_elements <= 1:
+    if num_elements <= 2:
+        # Doesn't matter
         return [ i+1 for i in range( num_elements ) ]
-    else: # Big list
+    else:
         n = num_elements
         sublist = median_sorted( n-1, median_of )
         insertion_point = ternary_search( sublist, n, 0, n-2, median_of )
@@ -18,7 +22,7 @@ def ternary_search( sublist, n, lo, hi, median_of ):
     """Returns zero-based index between lo and hi (inclusive) at which the number n should be inserted"""
     log( "ternary_search( %s, %s, %s )" % ( sublist, lo, hi ) )
     if hi-lo < 5:
-        # Who wants to think about corner cases
+        # Who wants to think about corner cases for small lists?
         log( " | j/k, let's do linear search" )
         result = linear_search( sublist, n, lo, hi, median_of )
         log( " | result = %s" % result )
@@ -42,48 +46,70 @@ def ternary_search( sublist, n, lo, hi, median_of ):
 
 def linear_search( sublist, n, lo, hi, median_of ):
     """sublist is a list of subscripts known to be sorted. n is a new subscript to add in there. lo and hi are zero-based indexes into sublist"""
-    #log( "linear_search( %s, %s, %s )" % ( sublist, lo, hi ) )
+    log( "linear_search( %s, %s, %s )" % ( sublist, lo, hi ) )
+    if lo == hi:
+        # Corner case: if hi == lo they refer to the same item, and we 
+        # can't tell them apart, making our whole algorithm break down!
+        # Just bump one of them
+        if lo >= 1:
+            log( "| widen lo=%s" % lo )
+            lo -= 1
+        else:
+            log( "| widen hi=%s" % hi )
+            hi += 1
     subscript_lo = sublist[ lo ]
     subscript_hi = sublist[ hi ]
     median = median_of( subscript_lo, n, subscript_hi )
     if median == subscript_lo:
-        #log( "| under %s" % lo )
+        log( "| under %s" % lo )
         return lo
     elif median == subscript_hi:
-        #log( "| over %s" % hi )
+        log( "| over %s" % hi )
         return hi+1
     elif hi - lo == 1:
-        #log( "| snug %s" % hi )
+        log( "| snug %s" % hi )
         return hi
     else:
-        #log( "| recurse" )
+        log( "| recurse" )
         return linear_search( sublist, n, lo+1, hi-1, median_of )
 
-def stdout_median( subscripts ):
+def stdout_median( *subscripts ):
+    log( "ASK: %s" % ( subscripts, ) )
     print( " ".join([ str(n) for n in subscripts ]) )
     stdout.flush()
-    return int( stdin.readline().strip() )
+    result = int( stdin.readline().strip() )
+    log( " | Answer: %s" % result )
+    if result == -1:
+        raise ValueError # Judge has spoken
+    elif not result in subscripts:
+        raise AssertionError # Judge lied
+    return result
 
 def mock_median_function( values ):
-    def median( i,j,k ):
-        log( "  median(%s,%s,%s)" % ( i,j,k ) )
-        subset = [ ( values[i-1], i ) for i in [i,j,k] ]
+    def median( *args ):
+        #log( "  median(%s)" % args )
+        subset = [ ( values[i-1], i ) for i in args ]
         ( _, result ) = sorted( subset )[1]
-        log( "  | subset %s -> %s" % ( subset, result ) )
+        #log( "  | subset %s -> %s" % ( subset, result ) )
         return result
     return median
 
 def main():
-    ( num_tests, num_elements, question_budget ) = [ int(v) for v in stdin.readline().strip().split() ]
+    parameters = [ int(v) for v in stdin.readline().strip().split() ]
+    log( "Parameters: T=%s N=%s Q=%s" % tuple(parameters) )
+    [ num_tests, num_elements, question_budget ] = parameters
     for i in range( num_tests ):
-        result = median_sorted( num_elements )
+        result = median_sorted( num_elements, stdout_median )
+        log( "SOLUTION: %s" % result )
         print( " ".join([ str(n) for n in result ]) )
         stdout.flush()
+        if stdin.readline().strip() != "1":
+            raise ValueError # Wrong answer
 
 def test():
     numbers = [ int(v) for v in argv[1:] ]
     print(median_sorted( len(numbers), mock_median_function( numbers ) ))
 
-#main()
-test()
+main()
+#test()
 stdout.flush()
